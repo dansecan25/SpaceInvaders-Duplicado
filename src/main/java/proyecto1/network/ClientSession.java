215b831087d3f088e9036860756a2ea0b0cb8ac5
adri <adriana@sinapsis.ws>
2021-05-03 16:18:46 -0600

    server communication with socket

--- src/main/java/proyecto1/network/ClientSession.java
+++ src/main/java/proyecto1/network/ClientSession.java
@@ -1,8 +1,9 @@
 package proyecto1.network;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.Buffer;
+import java.util.Date;
 
 public class ClientSession implements Runnable {
     private Socket socket;
@@ -15,10 +16,49 @@ public class ClientSession implements Runnable {
     @Override
     public void run() {
         OutputStream os = null;
+        //DataOutputStream dos;
+        OutputStreamWriter osw;
+        BufferedWriter bw;
+        InputStream is = null;
+        //DataInputStream dis;
+        InputStreamReader isr;
+        BufferedReader br;
         try {
             os = socket.getOutputStream();
+            //dos = new DataOutputStream(os);
+            osw = new OutputStreamWriter(os);
+            bw = new BufferedWriter(osw);
+            is = socket.getInputStream();
+            //dis = new DataInputStream(is);
+            isr = new InputStreamReader(is);
+            br = new BufferedReader(isr);
+
+
             do {
-                os.write("hola soy el server\n".getBytes());
+                bw.write("Ingrese comando: \n");
+                //dos.writeUTF("holi soy el server, ingrese un texto porfi: ");
+                bw.flush();
+                String command = br.readLine();
+                switch (command) {
+                    case "hola" : {
+                        bw.write("gracias por escribir hola \n" );
+                        bw.flush();
+                        break;
+                    }
+                    case "fecha" : {
+                        bw.write("hoy es" + new Date() + "\n");
+                        bw.flush();
+                        break;
+                    }
+                    default : {
+                        bw.write("no se como hacer " + command + "\n");
+                        bw.flush();
+                    }
+
+                }
+                bw.write("usted escribio: " + command + "\n");
+                bw.flush();
+                //dos.writeUTF("usted escribio: " + text);
                 Thread.sleep(500);
             } while(socket.isBound());
 
